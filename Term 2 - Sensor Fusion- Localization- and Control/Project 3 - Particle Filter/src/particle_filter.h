2dc129949ded69033cad7601bee14f1c70a0b65f
wisdomjuice <wisdomjuice+github@gmail.com>
2017-09-19 02:54:42 -0700

    Need to complete updateWeights

--- Term 2 - Sensor Fusion- Localization- and Control/Project 3 - Particle Filter/src/particle_filter.h
+++ Term 2 - Sensor Fusion- Localization- and Control/Project 3 - Particle Filter/src/particle_filter.h
@@ -11,8 +11,8 @@
 
 #include "helper_functions.h"
 
-struct Particle {
-
+struct Particle
+{
 	int id;
 	double x;
 	double y;
@@ -23,23 +23,19 @@ struct Particle {
 	std::vector<double> sense_y;
 };
 
+class ParticleFilter
+{
 
-
-class ParticleFilter {
-	
 	// Number of particles to draw
-	int num_particles; 
-	
-	
-	
+	int num_particles;
+
 	// Flag, if filter is initialized
 	bool is_initialized;
-	
+
 	// Vector of weights of all particles
 	std::vector<double> weights;
-	
+
 public:
-	
 	// Set of current particles
 	std::vector<Particle> particles;
 
@@ -71,15 +67,15 @@ public:
 	 * @param yaw_rate Yaw rate of car from t to t+1 [rad/s]
 	 */
 	void prediction(double delta_t, double std_pos[], double velocity, double yaw_rate);
-	
+
 	/**
 	 * dataAssociation Finds which observations correspond to which landmarks (likely by using
 	 *   a nearest-neighbors data association).
 	 * @param predicted Vector of predicted landmark observations
 	 * @param observations Vector of landmark observations
 	 */
-	void dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations);
-	
+	void dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs> &observations);
+
 	/**
 	 * updateWeights Updates the weights for each particle based on the likelihood of the 
 	 *   observed measurements. 
@@ -89,8 +85,8 @@ public:
 	 * @param map Map class containing map landmarks
 	 */
 	void updateWeights(double sensor_range, double std_landmark[], const std::vector<LandmarkObs> &observations,
-			const Map &map_landmarks);
-	
+										 const Map &map_landmarks);
+
 	/**
 	 * resample Resamples from the updated set of particles to form
 	 *   the new set of particles.
@@ -102,7 +98,7 @@ public:
 	 * This can be a very useful debugging tool to make sure transformations are correct and assocations correctly connected
 	 */
 	Particle SetAssociations(Particle particle, std::vector<int> associations, std::vector<double> sense_x, std::vector<double> sense_y);
-	
+
 	std::string getAssociations(Particle best);
 	std::string getSenseX(Particle best);
 	std::string getSenseY(Particle best);
@@ -110,11 +106,10 @@ public:
 	/**
 	 * initialized Returns whether particle filter is initialized yet or not.
 	 */
-	const bool initialized() const {
+	const bool initialized() const
+	{
 		return is_initialized;
 	}
 };
-
-
 
 #endif /* PARTICLE_FILTER_H_ */
